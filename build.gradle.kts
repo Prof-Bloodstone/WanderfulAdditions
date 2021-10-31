6137b43d1494c07dde9cd0781e0e86339f56682c
Professor Bloodstone <git@bloodstone.dev>
2021-10-31 17:28:11 +0100

    Fix building and properly shade impl

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,9 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ConfigureShadowRelocatio
 
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+    id("org.jetbrains.kotlin.jvm") version "1.5.20"
     id("com.diffplug.gradle.spotless") version "3.29.0"
-    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 description = "Provides wonderful wands for your Paper/Spigot server."
@@ -24,37 +24,23 @@ repositories {
     maven("https://repo.codemc.org/repository/maven-public") //bstats
 }
 
-/**
- * Special configuration to be included in resulting shadowed jar, but not added to the generated pom and gradle
- * metadata files.
- */
-val shadowImplementation by configurations.creating
-configurations["compileOnly"].extendsFrom(shadowImplementation)
-configurations["testImplementation"].extendsFrom(shadowImplementation)
-
 dependencies {
     compileOnly(group = "org.spigotmc", name = "spigot-api", version = mcVersion)
-    shadowImplementation(kotlin("stdlib", KotlinCompilerVersion.VERSION))
-    shadowImplementation(group = "co.aikar", name = "acf-paper", version = "0.5.0-SNAPSHOT")
-    shadowImplementation(group = "dev.bloodstone", name = "mcutils", version = "0.0.2")
-    shadowImplementation(group = "ch.jalu", name = "configme", version = "1.1.0")
-    shadowImplementation(group = "org.bstats", name = "bstats-bukkit", version = "1.7")
+    implementation(kotlin("stdlib", KotlinCompilerVersion.VERSION))
+    implementation(group = "co.aikar", name = "acf-paper", version = "0.5.0-SNAPSHOT")
+    implementation(group = "dev.bloodstone", name = "mcutils", version = "0.0.3")
+    implementation(group = "ch.jalu", name = "configme", version = "1.1.0")
+    implementation(group = "org.bstats", name = "bstats-bukkit", version = "1.7")
 }
 
-val shadowJarTask = tasks.named<ShadowJar>("shadowJar")
-val relocateShadowJarTask = tasks.register<ConfigureShadowRelocation>("relocateShadowJar") {
-    target = shadowJarTask.get()
-    prefix = "${rootProject.property("group")}.${rootProject.property("name").toString().toLowerCase()}.lib"
-}
-
-shadowJarTask.configure {
-    dependsOn(relocateShadowJarTask)
-    minimize()
-    archiveClassifier.set("")
-    configurations = listOf(shadowImplementation)
-}
 
 tasks {
+
+    shadowJar {
+        dependsOn(named("relocateShadowJar"))
+        minimize()
+        archiveClassifier.set("")
+    }
     build {
         dependsOn(shadowJar)
     }
@@ -88,5 +74,10 @@ tasks {
             expand("version" to rootProject.version)
         }
     }
+}
+
+task<ConfigureShadowRelocation>("relocateShadowJar") {
+    target = tasks.shadowJar.get()
+    prefix = "${rootProject.property("group")}.${rootProject.property("name").toString().toLowerCase()}.lib"
 }
 
